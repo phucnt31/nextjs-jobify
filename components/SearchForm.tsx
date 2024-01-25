d870c04464eeb06c06444c425dad63e1cff44f4f
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-01-25 18:47:49 +0700

    SearchForm component functionality

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -14,12 +14,23 @@ import {
 import { JobStatus } from "@/utils/types";
 
 const SearchForm = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const search = searchParams.get("search") || "";
+  const jobStatus = searchParams.get("jobStatus") || "all";
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const search = formData.get("search") as string;
     const jobStatus = formData.get("jobStatus") as string;
-    console.log(search, jobStatus);
+
+    let params = new URLSearchParams();
+    params.set("search", search);
+    params.set("jobStatus", jobStatus);
+
+    router.push(`${pathname}?${params.toString()}`);
   };
 
   return (
@@ -27,8 +38,13 @@ const SearchForm = () => {
       onSubmit={handleSubmit}
       className="bg-muted mb-16 p-8 grid sm:grid-cols-2 md:grid-cols-3 gap-4 rounded-lg"
     >
-      <Input type="text" placeholder="Search Jobs" name="search" />
-      <Select name="jobStatus">
+      <Input
+        type="text"
+        placeholder="Search Jobs"
+        name="search"
+        defaultValue={search}
+      />
+      <Select name="jobStatus" defaultValue={jobStatus}>
         <SelectTrigger>
           <SelectValue />
         </SelectTrigger>
