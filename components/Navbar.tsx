f7c43f3aa093475f60b7f6d97e651fcd1d960d8c
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-01-25 10:32:17 +0700

    Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import LinksDropdown from "./LinksDropdown";
+import { UserButton } from "@clerk/nextjs";
+import ThemeToggle from "./ThemeToggle";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="bg-muted py-4 sm:px-16 lg:px-24 px-4 flex items-center justify-between">
+      <LinksDropdown />
+      <div className="flex items-center gap-x-4">
+        <ThemeToggle />
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
