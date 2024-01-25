638f4b10fa76e32c276dd21742d013135e3fb600
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-01-25 09:40:57 +0700

    Layout and Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,32 @@
 import { Button } from "@/components/ui/button";
-import { Camera } from "lucide-react";
+import Image from "next/image";
+import Logo from "../assets/logo.svg";
+import LandingImg from "../assets/main.svg";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <Button>default button</Button>
-      <Button variant="outline" size="icon">
-        <Camera />
-      </Button>
-    </div>
+    <main>
+      <header className="max-w-6xl mx-auto px-4 sm:px-8 py-6">
+        <Image src={Logo} alt="logo" />
+        <section className="max-w-6xl mx-auto px-4 sm:px-8 h-screen -mt-20 grid lg:grid-cols-[1fr,400px] items-center">
+          <div>
+            <h1 className="capitalize text-4xl md:text-7xl font-bold">
+              job <span className="text-primary">tracking</span> app
+            </h1>
+            <p className="leading-loose max-w-md mt-4">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut
+              tenetur magnam praesentium? Saepe, explicabo labore assumenda
+              doloribus molestiae earum adipisci? A aliquid deleniti aliquam
+              adipisci sapiente doloribus ratione sed odio?
+            </p>
+            <Button asChild className="mt-4">
+              <Link href="/add-job">Get Started</Link>
+            </Button>
+          </div>
+          <Image src={LandingImg} alt="landing" className="hidden lg:block" />
+        </section>
+      </header>
+    </main>
   );
 }
