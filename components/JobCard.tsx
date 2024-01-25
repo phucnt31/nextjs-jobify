720db94fc2d2a25f7ccd27348f64568e4b958668
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-01-25 19:06:39 +0700

    Setup JobCard component

--- components/JobCard.tsx
+++ components/JobCard.tsx
@@ -1,8 +1,39 @@
 import React from "react";
 import { JobType } from "@/utils/types";
+import { MapPin, Briefcase, CalendarDays, RadioTower } from "lucide-react";
+
+import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
+import { Badge } from "./ui/badge";
+import JobInfo from "./JobInfo";
+import DeleteJobButton from "./DeleteJobBtn";
 
 const JobCard = ({ job }: { job: JobType }) => {
-  return <div>JobCard</div>;
+  return (
+    <Card className="bg-muted">
+      <CardHeader>
+        <CardTitle>{job.position}</CardTitle>
+        <CardDescription>{job.company}</CardDescription>
+      </CardHeader>
+      <Separator />
+      <CardContent></CardContent>
+      <CardFooter className="flex gap-4">
+        <Button asChild size="sm">
+          <Link href={`/jobs/${job.id}`}>edit</Link>
+        </Button>
+        <DeleteJobButton />
+      </CardFooter>
+    </Card>
+  );
 };
 
 export default JobCard;
