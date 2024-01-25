9da26d2a91990cba7ed090f0b388cb41f84a0550
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-01-25 13:59:12 +0700

    Add React Query and Toaster

--- app/(dashboard)/add-job/page.tsx
+++ app/(dashboard)/add-job/page.tsx
@@ -1,11 +1,18 @@
 import CreateJobForm from "@/components/CreateJobForm";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 import React from "react";
 
 const AddJobPage = () => {
+  const queryClient = new QueryClient();
+
   return (
-    <>
+    <HydrationBoundary state={dehydrate(queryClient)}>
       <CreateJobForm />
-    </>
+    </HydrationBoundary>
   );
 };
 
--- app/providers.tsx
+++ app/providers.tsx
@@ -2,8 +2,22 @@
 
 import React from "react";
 import { ThemeProvider } from "@/components/theme-provider";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "@/components/ui/toaster";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(() => {
+    return new QueryClient({
+      defaultOptions: {
+        queries: {
+          staleTime: 60 * 1000 * 5,
+        },
+      },
+    });
+  });
+
   return (
     <>
       <ThemeProvider
@@ -12,7 +26,11 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
         enableSystem
         disableTransitionOnChange
       >
-        {children}
+        <Toaster />
+        <QueryClientProvider client={queryClient}>
+          {children}
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
       </ThemeProvider>
     </>
   );
